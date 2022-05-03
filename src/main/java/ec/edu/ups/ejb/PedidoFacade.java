6b937707b9c355fa235d5a285b2740b9e6d2b896
Ivàn Sànchez <isanchezg1@est.ups.edu.ec>
2022-05-03 14:11:13 -0500

    Creación de PedidoFacade.

---
+++ src/main/java/ec/edu/ups/ejb/PedidoFacade.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.ejb;
+
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author rolan
+ */
+@Stateless
+public class PedidoFacade {
+    
+}
