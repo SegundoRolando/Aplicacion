402f9b69966d91bba608669a2f7155a49b9369ae
SegundoRolando <scallet@est.ups.edu.ec>
2022-05-14 07:54:29 -0500

    Tabla Proveedores

---
+++ src/main/java/ec/edu/ups/entidades/Proveedor.java
@@ -0,0 +1,110 @@
+package ec.edu.ups.entidades;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author ASUS
+ */
+@Entity
+public class Proveedor implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nom_Proveedor;
+    private String correo;
+    private String telefono;
+    private String direccion;
+    private String ciudad;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int id, String nom_Proveedor, String correo, String telefono, String direccion, String ciudad) {
+        this.id = id;
+        this.nom_Proveedor = nom_Proveedor;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.ciudad = ciudad;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom_Proveedor() {
+        return nom_Proveedor;
+    }
+
+    public void setNom_Proveedor(String nom_Proveedor) {
+        this.nom_Proveedor = nom_Proveedor;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (int) id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Proveedor)) {
+            return false;
+        }
+        Proveedor other = (Proveedor) object;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ec.edu.ups.entidades.Proveedor[ id=" + id + " ]";
+    }
+    
+}
