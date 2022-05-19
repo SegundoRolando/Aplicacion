a7490839ba3bac63918822c20b96e568b8532ad4
Ivàn Sànchez <isanchezg1@est.ups.edu.ec>
2022-05-18 23:01:05 -0500

    Crud Categoria-Producto-Asociacion-Terminado.

--- src/main/java/ec/edu/ups/entidades/Categoria.java
+++ src/main/java/ec/edu/ups/entidades/Categoria.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.util.Objects;
 
 /**
  *
@@ -39,6 +40,14 @@ public class Categoria {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
     }
 
     @Override
