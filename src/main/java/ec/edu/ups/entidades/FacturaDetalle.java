05fec95c0adba4978cd4add4339084e6339e0dab
Ivàn Sànchez <isanchezg1@est.ups.edu.ec>
2022-05-31 17:39:18 -0500

    Entidad Detalle

---
+++ src/main/java/ec/edu/ups/entidades/FacturaDetalle.java
@@ -0,0 +1,94 @@
+package ec.edu.ups.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+/**
+ *
+ * @author rolan
+ */
+@Entity
+public class FacturaDetalle {
+    @Id
+    private Long id;
+    private int cantidad;
+    private double precio;
+    private String descripcion;
+    private double totalXproducto;
+    private Producto producto;
+
+    public FacturaDetalle() {
+    }
+
+    public FacturaDetalle(Long id, int cantidad, double precio, String descripcion, double totalXproducto) {
+        this.id = id;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.descripcion = descripcion;
+        this.totalXproducto = totalXproducto;
+    }
+
+    
+    
+    public FacturaDetalle(Long id, int cantidad, double precio, double totalXproducto, Producto producto) {
+        this.id = id;
+        this.cantidad = cantidad;
+        this.precio = precio;
+        this.totalXproducto = totalXproducto;
+        this.producto = producto;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getTotalXproducto() {
+        return totalXproducto;
+    }
+
+    public void setTotalXproducto(double totalXproducto) {
+        this.totalXproducto = totalXproducto;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    @Override
+    public String toString() {
+        return "FacturaDeatalle{" + "id=" + id + ", cantidad=" + cantidad + ", precio=" + precio + ", totalXproducto=" + totalXproducto + ", producto=" + producto + '}';
+    }
+    
+}
